9099ba6b759a1f4e7165912e1253811cff764f75
Kasheena Walton <kasheenaw@gmail.com>
2018-09-18 21:16:01 -0700

    text site

--- build.py
+++ build.py
@@ -1,2 +1,10 @@
-test change
-this is a test change
+templates = open('templates/top.html').read()
+about_me = open('content/blog2.txt').read()
+contact = open('content/contact2.txt').read()
+content = open('content/blog1.txt').read()
+templates = open('templates/bottom.html').read()
+html_site = templates + about_me + contact + content + templates
+print(html_site)
+
+
+
